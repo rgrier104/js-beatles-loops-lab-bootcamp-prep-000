ec1d4121562ba857df0a8e84059de78c3f0dd75c
Rachel Grier <rgrier104@gmail.com>
2018-08-01 01:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function theBeatlesPlay(musicians,instruments) {
   var empty = []
   for (let i = 0; i < musicians.length; i++) {
     empty.push((`musician[i] plays instruments[i]`))
-  }
+  } return empty
 }
